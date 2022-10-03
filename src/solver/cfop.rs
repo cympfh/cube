52d20f6ee1ba71858f0d4ffc4f95ea00fa317333
cympfh <cympfh@gmail.com>
2022-10-03 22:52:48 +0900

    Revert "exec F2L#1+2 (#28)"
    This reverts commit e2ecd2c1c8162a557b50b9eeeb57cdb881129793.

--- src/solver/cfop.rs
+++ src/solver/cfop.rs
@@ -72,10 +72,8 @@ pub fn cfop(cube: &Cube, verbose: bool) -> Option<Ops> {
         Right(false),
         Left(true),
         Left(false),
-        Middle(true),
-        Middle(false),
     ];
-    match search_one(&subcube, &subgoal, allowed_ops, 4, verbose) {
+    match search_one(&subcube, &subgoal, allowed_ops, 5, verbose) {
         Some(alg) => {
             algorithm.extend(&alg);
             cube = alg.apply(&cube);
@@ -85,17 +83,17 @@ pub fn cfop(cube: &Cube, verbose: bool) -> Option<Ops> {
         }
     }
 
-    info!("F2L/#1+2");
+    info!("F2L/#1");
     let subgoal = cube![
         . . . ;
         . Y . ;
         . . . ;
         . . . . . . . . . . . . ;
-        R R . . G . . O O B B B ;
-        R R . . G . . O O B B B ;
+        R R . . G . . O . . B B ;
+        R R . . G . . O . . B B ;
         W W . ;
         W W W ;
-        W W . ;
+        . W . ;
     ];
     let mut subcube = cube.clone();
     subcube.mask(&subgoal);
@@ -117,74 +115,41 @@ pub fn cfop(cube: &Cube, verbose: bool) -> Option<Ops> {
             cube = alg.apply(&cube);
         }
         None => {
-            info!("F2L/#1");
-            let subgoal = cube![
-                . . . ;
-                . Y . ;
-                . . . ;
-                . . . . . . . . . . . . ;
-                R R . . G . . O . . B B ;
-                R R . . G . . O . . B B ;
-                W W . ;
-                W W W ;
-                . W . ;
-            ];
-            let mut subcube = cube.clone();
-            subcube.mask(&subgoal);
-            let allowed_ops = vec![
-                Front(true),
-                Front(false),
-                Back(true),
-                Back(false),
-                Up(true),
-                Up(false),
-                Right(true),
-                Right(false),
-                Left(true),
-                Left(false),
-            ];
-            match search_one(&subcube, &subgoal, allowed_ops, 6, verbose) {
-                Some(alg) => {
-                    algorithm.extend(&alg);
-                    cube = alg.apply(&cube);
-                }
-                None => {
-                    return None;
-                }
-            }
-            info!("F2L/#2");
-            let subgoal = cube![
-                . . . ;
-                . Y . ;
-                . . . ;
-                . . . . . . . . . . . . ;
-                R R . . G . . O O B B B ;
-                R R . . G . . O O B B B ;
-                W W . ;
-                W W W ;
-                W W . ;
-            ];
-            let mut subcube = cube.clone();
-            subcube.mask(&subgoal);
-            let allowed_ops = vec![
-                Back(true),
-                Back(false),
-                Up(true),
-                Up(false),
-                Right(true),
-                Right(false),
-                Left(true),
-                Left(false),
-            ];
-            match search_one(&subcube, &subgoal, allowed_ops, 6, verbose) {
-                Some(alg) => {
-                    algorithm.extend(&alg);
-                    cube = alg.apply(&cube);
-                }
-                None => {
-                    return None;
-                }
-            }
+            return None;
+        }
+    }
+
+    info!("F2L/#2");
+    let subgoal = cube![
+        . . . ;
+        . Y . ;
+        . . . ;
+        . . . . . . . . . . . . ;
+        R R . . G . . O O B B B ;
+        R R . . G . . O O B B B ;
+        W W . ;
+        W W W ;
+        W W . ;
+    ];
+    let mut subcube = cube.clone();
+    subcube.mask(&subgoal);
+    let allowed_ops = vec![
+        Back(true),
+        Back(false),
+        Up(true),
+        Up(false),
+        Right(true),
+        Right(false),
+        Left(true),
+        Left(false),
+    ];
+    match search_one(&subcube, &subgoal, allowed_ops, 8, verbose) {
+        Some(alg) => {
+            algorithm.extend(&alg);
+            cube = alg.apply(&cube);
+        }
+        None => {
+            return None;
         }
     }
 
